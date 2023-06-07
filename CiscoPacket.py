af91638710fe1707abcb2e28214b0e503ac1ff17
DmitryAnglichanin <dimas.pulyakhin@gmail.com>
2023-06-07 19:40:56 +0300

    Пофиксил диалоговое окно

--- CiscoPacket.py
+++ CiscoPacket.py
@@ -1,8 +1,11 @@
 import sys
 from PyQt5.QtWidgets import QApplication, QGraphicsSceneMouseEvent, QMainWindow, QDesktopWidget, QPushButton, QGraphicsScene, QGraphicsView, \
-    QTableWidget, QTableWidgetItem, QInputDialog, QGraphicsRectItem, QLabel, QLineEdit, QVBoxLayout, QDialog
-from PyQt5.QtGui import QColor, QBrush, QPen, QPainter, QMouseEvent
-from PyQt5.QtCore import Qt, QPointF, QRectF, pyqtSignal
+    QTableWidget, QTableWidgetItem, QInputDialog, QGraphicsRectItem, QLabel, QLineEdit, QVBoxLayout, QDialog, QComboBox
+from PyQt5.QtGui import QColor, QBrush, QPen, QPainter
+from PyQt5.QtCore import Qt, QPointF, QRectF, pyqtSignal, QTimer, pyqtSlot
+from PyQt5.QtWidgets import QGraphicsItem, QGraphicsLineItem
+
+from dialogs import RouterDialog, PCDialog, SwitchDialog
 
 
 class Object(QGraphicsRectItem):
@@ -11,22 +14,51 @@ class Object(QGraphicsRectItem):
         self.setFlag(self.ItemIsMovable)
         self.setPen(pen)
         self.setBrush(brush)
+        self.connections = []
 
     def get_type(self) -> str:
         return self.type
+
+    def add_connection(self, connection):
+        self.connections.append(connection)
+
+    def remove_connection(self, connection):
+        self.connections.remove(connection)
+
+    def update_connections(self):
+        for connection in self.connections:
+            connection.update()
+
+    def itemChange(self, change, value):
+        if change == QGraphicsItem.ItemPositionChange:
+            self.update_connections()
+        return super().itemChange(change, value)
+
+
+class Connection(QGraphicsLineItem):
+    def __init__(self, start_item, end_item):
+        super().__init__()
+        self.start_item = start_item
+        self.end_item = end_item
+        self.start_item.add_connection(self)
+        self.end_item.add_connection(self)
+        self.update()
+
+    def update(self):
+        start_pos = self.start_item.pos() + QPointF(self.start_item.rect().width() / 2,
+                                                    self.start_item.rect().height() / 2)
+        end_pos = self.end_item.pos() + QPointF(self.end_item.rect().width() / 2,
+                                                self.end_item.rect().height() / 2)
+        self.setLine(start_pos.x(), start_pos.y(), end_pos.x(), end_pos.y())
 
 
 class Router(Object):
     def __init__(self):
         super().__init__(QRectF(QPointF(0, 0), QPointF(30, 30)), QPen(Qt.black), QBrush(Qt.gray))
-        self.dialog = QDialog()
+        self.dialog = RouterDialog()
         self.type = "router"
-        self.label = QLabel("Router Dialog")
-        layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        self.dialog.setLayout(layout)
 
-    def mouseDoubleClickEvent(self, event: QGraphicsSceneMouseEvent) -> None:
+    def mouseDoubleClickEvent(self, event):
         self.dialog.exec()
 
 
@@ -34,48 +66,20 @@ class Switch(Object):
     def __init__(self):
         super().__init__(QRectF(QPointF(0, 0), QPointF(50, 30)), QPen(Qt.yellow), QBrush(Qt.white))
         self.type = "switch"
-        self.dialog = QDialog()
-        self.label = QLabel("Switch Dialog")
-        layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        self.dialog.setLayout(layout)
+        self.dialog = SwitchDialog()
 
-    def mouseDoubleClickEvent(self, event: QGraphicsSceneMouseEvent) -> None:
+    def mouseDoubleClickEvent(self, event: QGraphicsSceneMouseEvent):
         self.dialog.exec()
 
 
 class PC(Object):
     def __init__(self):
         super().__init__(QRectF(QPointF(0, 0), QPointF(30, 30)), QPen(Qt.black), QBrush(Qt.yellow))
-        self.dialog = QDialog()
+        self.dialog = PCDialog()
         self.type = "pc"
-        self.label = QLabel("PC Dialog")
-        layout = QVBoxLayout()
-        layout.addWidget(self.label)
-        self.dialog.setLayout(layout)
 
-    def mouseDoubleClickEvent(self, event: QGraphicsSceneMouseEvent) -> None:
+    def mouseDoubleClickEvent(self, event: QGraphicsSceneMouseEvent):
         self.dialog.exec()
-
-
-class Connection(QGraphicsRectItem):
-    def __init__(self, start_item, end_item):
-        super().__init__(QRectF())
-        self.start_item = start_item
-        self.end_item = end_item
-        self.setZValue(-1)  # Установка z-позиции ниже объектов
-        self.pen = QPen(Qt.red)
-        self.pen.setWidth(2)
-        self.setPen(self.pen)
-
-    def update(self):
-        start_pos = self.start_item.pos() + QPointF(self.start_item.rect().width() / 2, self.start_item.rect().height() / 2)
-        end_pos = self.end_item.pos() + QPointF(self.end_item.rect().width() / 2, self.end_item.rect().height() / 2)
-        self.setRect(QRectF(start_pos, end_pos))
-
-    def paint(self, painter, option, widget=None):
-        painter.setRenderHint(QPainter.Antialiasing)
-        super().paint(painter, option, widget)
 
 
 def create_object_of_type(type: str) -> Object:
@@ -86,6 +90,30 @@ def create_object_of_type(type: str) -> Object:
     elif type == 'pc':
         obj = PC()
     return obj
+
+
+class MyView(QGraphicsView):
+    def __init__(self, scene, parent=None):
+        super().__init__(scene, parent)
+        self.setRenderHint(QPainter.Antialiasing)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            item = self.itemAt(event.pos())
+            if isinstance(item, Object):
+                item.setFlag(QGraphicsItem.ItemIsMovable, True)
+                item.setFlag(QGraphicsItem.ItemSendsScenePositionChanges, True)
+
+        super().mousePressEvent(event)
+
+    def mouseReleaseEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            item = self.itemAt(event.pos())
+            if isinstance(item, Object):
+                item.setFlag(QGraphicsItem.ItemIsMovable, False)
+                item.setFlag(QGraphicsItem.ItemSendsScenePositionChanges, False)
+
+        super().mouseReleaseEvent(event)
 
 
 class MyWindow(QMainWindow):
@@ -105,7 +133,7 @@ class MyWindow(QMainWindow):
 
         # Создаем сцену для отрисовки объектов
         self.scene = QGraphicsScene(self)
-        self.view = QGraphicsView(self.scene, self)
+        self.view = MyView(self.scene, self)
         self.view.setGeometry(0, 0, width, height)
         self.view.setHorizontalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
         self.view.setVerticalScrollBarPolicy(Qt.ScrollBarAlwaysOff)
@@ -124,8 +152,9 @@ class MyWindow(QMainWindow):
         self.add_pc_button.clicked.connect(lambda: self.add_object('pc'))
         self.add_pc_button.setGeometry(10, 150, 150, 30)
 
-        # Создаем список объектов и связей
+        # Создаем список объектов
         self.objects = []
+
         self.connections = []
         self.current_connection = None
 
@@ -137,29 +166,29 @@ class MyWindow(QMainWindow):
         self.objects.append(obj)
 
     def mousePressEvent(self, event):
-        if event.button() == Qt.RightButton:
+        if event.button() == Qt.LeftButton:
             obj = self.scene.itemAt(event.pos(), self.view.transform())
             if isinstance(obj, Object):
-                if self.current_connection is None:
-                    # Если нет активной связи, выбираем объект начала связи
-                    obj.setFlag(QGraphicsRectItem.ItemIsMovable, False)
-                    self.current_connection = Connection(obj, None)
-                    self.connections.append(self.current_connection)
-                    self.scene.addItem(self.current_connection)
-                else:
-                    # Если уже выбран объект начала связи, выбираем объект конца связи
-                    self.current_connection.end_item = obj
-                    self.current_connection.update()
-                    self.current_connection = None
-                    obj.setFlag(QGraphicsRectItem.ItemIsMovable, True)
+                self.current_connection = Connection(obj, None)
+                self.connections.append(self.current_connection)
+
+    def mouseMoveEvent(self, event):
+        if self.current_connection:
+            self.current_connection.update()
 
     def mouseReleaseEvent(self, event):
-        if event.button() == Qt.RightButton and self.current_connection is not None:
-            # Если связь была начата, но не завершена при отпускании кнопки мыши, удаляем связь
-            self.scene.removeItem(self.current_connection)
-            self.current_connection.start_item.setFlag(QGraphicsRectItem.ItemIsMovable, True)
-            self.connections.remove(self.current_connection)
-            self.current_connection = None
+        if event.button() == Qt.LeftButton:
+            obj = self.scene.itemAt(event.pos(), self.view.transform())
+            if isinstance(obj, Object) and self.current_connection:
+                self.current_connection.end_item = obj
+                self.current_connection.update()
+                self.current_connection = None
+            elif self.current_connection in self.connections:
+                self.scene.removeItem(self.current_connection)
+                self.current_connection.start_item.remove_connection(self.current_connection)
+                self.current_connection.end_item.remove_connection(self.current_connection)
+                self.connections.remove(self.current_connection)
+                self.current_connection = None
 
 
 if __name__ == '__main__':
